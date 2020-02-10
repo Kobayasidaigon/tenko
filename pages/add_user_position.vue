f36b87728490f0fb5761ea90fbd53a3523c5a5e3
Kobayashidaigon <hal.60445.kobayashi@gmail.com>
2020-02-10 14:47:07 +0900

    の追加firebaseの追加

---
+++ pages/add_user_position.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-btn @click="add_user">ユーザー現在地取得</v-btn>
+  </div>
+</template>
+<script>
+import firebase from "~/plugins/firebase.js";
+const db = firebase.firestore();
+export default {
+  methods: {
+    add_user() {
+      navigator.geolocation.getCurrentPosition(success, err);
+      function success(position) {
+        console.log(position);
+      }
+      function err(e) {
+        console.log(e);
+      }
+      // db.collection('user_position').add({
+      //     user:"大悟"
+      // });
+    }
+  }
+};
+</script>
---
+++ plugins/firebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
+// Your web app's Firebase configuration
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
