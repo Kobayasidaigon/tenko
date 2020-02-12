b5e1baf793957b6b997ba3cd724caf1f5636397b
Kobayashidaigon <hal.60445.kobayashi@gmail.com>
2020-02-12 12:47:10 +0900

    firebaseの追加

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,7 +2,16 @@ import firebase from "firebase";
 import "firebase/firestore";
 import "firebase/auth";
 // Your web app's Firebase configuration
-
+var firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: 'train-77065',
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.measurementId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
