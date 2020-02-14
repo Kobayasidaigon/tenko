01de8c2ac7ca28453be931aa74464890f6172592
Kobayashidaigon <hal.60445.kobayashi@gmail.com>
2020-02-14 20:25:34 +0900

    記述の変更

--- pages/contorol_page.vue
+++ pages/contorol_page.vue
@@ -14,14 +14,12 @@
 
     <v-card max-width="500" class="mx-auto">
       <v-toolbar color="deep-purple accent-4" dark>
-        <v-toolbar-title>New Chat</v-toolbar-title>
+        <v-toolbar-title>ユーザー一覧</v-toolbar-title>
 
         <v-spacer></v-spacer>
       </v-toolbar>
 
       <v-list subheader>
-        <v-subheader>Recent chat</v-subheader>
-
         <v-list-item v-for="item in this.user_list" :key="item.name" >
           <v-list-item-avatar>
             <v-img :src="item.avatar"></v-img>
@@ -33,22 +31,6 @@
 
         </v-list-item>
       </v-list>
-
-      <v-divider></v-divider>
-
-      <v-list subheader>
-        <v-subheader>Previous chats</v-subheader>
-
-        <v-list-item v-for="item in items2" :key="item.title" >
-          <v-list-item-avatar>
-            <v-img :src="item.avatar"></v-img>
-          </v-list-item-avatar>
-
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
     </v-card>
   </v-col>
 </template>
@@ -57,32 +39,6 @@ import firebase from "~/plugins/firebase.js";
 const db = firebase.firestore();
 export default {
   data: () => ({
-    items: [
-      {
-        active: true,
-        title: "Jason Oner",
-        avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg"
-      },
-      {
-        active: true,
-        title: "Ranee Carlson",
-        avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg"
-      },
-      {
-        title: "Cindy Baker",
-        avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg"
-      },
-      {
-        title: "Ali Connors",
-        avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg"
-      }
-    ],
-    items2: [
-      {
-        title: "Travis Howard",
-        avatar: "https://cdn.vuetifyjs.com/images/lists/5.jpg"
-      }
-    ],
     login_id: "",
     user_list:[]
   }),
